5bb2b2fb600f2b2c8cdd046d5759111a7821da33
khayalnb <xeyal.binnetov@profit.az>
2023-10-12 21:20:17 +0400

    regex validation v1

---
+++ RegEx-Validation/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace RegEx_Validation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter password: ");
+            string password = Console.ReadLine();
+
+            Console.Write("Please enter password: ");
+            string mail = Console.ReadLine();
+
+
+            string resultValidMessage = RegExValidation.PasswordIsValid(password)
+                && RegExValidation.EmailIsValid(mail) ? 
+                "email and password is valid" : 
+                "email or password is invalid";
+
+            Console.WriteLine(resultValidMessage);
+
+        }
+    }
+}
---
+++ RegEx-Validation/RegExValidation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegEx_Validation
+{
+    public static class RegExValidation
+    {
+        public static bool PasswordIsValid(string userPassword)
+        {
+            string pattern= @"^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$";
+            Regex regex =new Regex(pattern);
+            var regaxResult = regex.Match(userPassword);
+            return regaxResult.Success is true ? true : false;
+        }
+
+        public static bool EmailIsValid(string userPassword)
+        {
+            string pattern = @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,})+)$";
+            Regex regex = new Regex(pattern);
+            var regaxResult = regex.Match(userPassword);
+            return regaxResult.Success is true ? true : false;
+        }
+    }
+}
